f7186b0f4f9ebb8a18e4c57745a18f3629df3446
JSands <jonathan.pw.sands@gmail.com>
2025-11-19 12:04:49 +0800

    Initial Vue 3 / Quasar / Vite Setup

---
+++ resources/js/Components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-btn label="Click Me" color="primary" />
+</template>
+
+<script>
+import { QBtn } from 'quasar'
+
+export default {
+  components: { QBtn }
+}
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
+<div id="app">
+    <example-component></example-component>
+</div>
+
+@vite('resources/js/app.js')
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -20,6 +26,7 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
