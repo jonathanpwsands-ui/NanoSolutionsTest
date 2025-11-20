5328244e43b9da42dee7cc16898bf5a041a33ac4
JSands <jonathan.pw.sands@gmail.com>
2025-11-20 22:56:16 +0800

    Dark Mode Toggle implemented

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,15 +3,36 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>Notes App</q-toolbar-title>
+
+        <!-- Dark Mode Toggle -->
+        <q-btn
+          flat
+          round
+          dense
+          icon="dark_mode"
+          @click="toggleDarkMode"
+        />
       </q-toolbar>
     </q-header>
 
+    <!-- Content -->
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
-export default { name: 'App' }
+import { Dark } from 'quasar';
+
+export default {
+  name: 'App',
+
+  methods: {
+    toggleDarkMode() {
+      Dark.toggle(); // 🌙 toggles dark/light
+    }
+  }
+};
 </script>
