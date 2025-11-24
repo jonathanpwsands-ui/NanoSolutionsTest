8fbf16c7b02cc4a9afc3f1330f978245e6f1e267
JSands <jonathan.pw.sands@gmail.com>
2025-11-24 16:19:39 +0800

    'user_id' added to Note model and migration as Foreign Key

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -9,5 +9,5 @@ class Note extends Model
 {
     use HasFactory;
     protected $table = 'notes';
-    protected $fillable = ['title','content'];
+    protected $fillable = ['title','content','user_id'];
 }
