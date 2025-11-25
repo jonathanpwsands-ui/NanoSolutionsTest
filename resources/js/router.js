0aece05debc06af92d5ed4ffc08ee0d98b8c5bd1
JSands <jonathan.pw.sands@gmail.com>
2025-11-25 16:50:17 +0800

    router.js updated with Login and Register Pages

--- resources/js/router.js
+++ resources/js/router.js
@@ -6,6 +6,8 @@ import EditNotePage from './pages/EditNotePage.vue';
 
 // List of visible routes
 const routes = [
+    { path: '/login', component: () => import('./pages/LoginPage.vue') }, // User login page
+    { path: '/register', component: () => import('./pages/RegisterPage.vue') }, // User registration page
     { path: '/', component: NoteListPage },                            // Main page
     { path: '/notes/create', component: CreateNotePage },               // Note creation page
     { path: '/notes/:id/edit', component: EditNotePage, props: true },  // Note editing page
