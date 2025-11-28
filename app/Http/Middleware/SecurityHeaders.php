eba4553dc80a0adc93ebcd59787113bbbfc41d76
Jonathan Sands <jonathan.pw.sands@gmail.com>
2025-11-28 11:35:06 +0800

    Content Security Policy added

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -15,6 +15,9 @@ class SecurityHeaders
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
         $response->headers->set('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
+        $response->headers->set('Content-Security-Policy', 
+            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+        );
         
         return $response;
     }
